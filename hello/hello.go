85724b658355cce4bda4dc3b823b3af51183cab3
norictech <ahmad.uji1902@gmail.com>
2021-10-13 17:02:26 +0700

    handle error

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/nor1c/go-play/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Fauzi")
-	fmt.Println(message)
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Fauzi")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message) // will return 'empty name' if name is not provided
 }
