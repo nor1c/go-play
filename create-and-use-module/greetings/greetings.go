c43d6aa517aaadbe0558e27be9f7c772ba28828c
norictech <ahmad.uji1902@gmail.com>
2021-10-14 09:48:56 +0700

    separate folder

--- create-and-use-module/greetings/greetings.go
+++ create-and-use-module/greetings/greetings.go
@@ -6,10 +6,12 @@ import (
 )
 
 func Hello(name string) (string, error) {
+	// if no name was given, return an error with a message.
 	if name == "" {
 		return "", errors.New("empty name")
 	}
 
+	// if there is no error, return value that embed the name
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message, nil
 }
