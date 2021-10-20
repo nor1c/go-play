b96127c172395795c24680e11948049f57a3af08
norictech <ahmad.uji1902@gmail.com>
2021-10-20 21:30:07 +0700

    working with recursion

--- recursion.go
+++ recursion.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
 func main() {
-	fmt.Println("test")
+	fmt.Println(fact(7)) // 7*(6*(5*(4*(3*(2*(1*(1)))))) = 5040
+
+	var fib func(n int) int
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+		return fib(n-1) + fib(n-2)
+	}
+	fmt.Println(fib(7)) // 13
 }
